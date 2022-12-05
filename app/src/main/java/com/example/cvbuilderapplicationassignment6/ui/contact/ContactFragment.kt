48080e4e2404979bcd002f74398ee8c7bd9fc939
DineshSarma1 <dineshsarma202@gmail.com>
2022-12-05 17:04:49 -0600

    emaining features are implemented

--- app/src/main/java/com/example/cvbuilderapplicationassignment6/ui/contact/ContactFragment.kt
+++ app/src/main/java/com/example/cvbuilderapplicationassignment6/ui/contact/ContactFragment.kt
@@ -1,12 +1,15 @@
 package com.example.cvbuilderapplicationassignment6.ui.contact
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.cvbuilderapplicationassignment6.MyWebActivity
 import com.example.cvbuilderapplicationassignment6.R
 
 
@@ -41,19 +44,25 @@ class ContactFragment : Fragment(R.layout.fragment_contact), OnClickListener {
     }
 
     private fun onEmailClicked() {
-
+        sendEmail()
     }
 
     private fun onLinkedInClicked() {
-
+        openWebUrl(getString(R.string.linkedin_profile))
     }
 
     private fun onGithubClicked() {
-
+        openWebUrl(getString(R.string.github_profile))
     }
 
     private fun onResumeClicked() {
 
+    }
+
+    private fun openWebUrl(url: String) {
+        val intent = Intent(requireActivity(), MyWebActivity::class.java)
+        intent.putExtra("url",url)
+        requireContext().startActivity(intent)
     }
 
     override fun onClick(view: View?) {
@@ -71,5 +80,28 @@ class ContactFragment : Fragment(R.layout.fragment_contact), OnClickListener {
         fun newInstance() = ContactFragment()
     }
 
+
+    private fun sendEmail() {
+        val isAppInstalled: Boolean = isAppAvailable("com.google.android.gm")
+        if (isAppInstalled) {
+            val myIntent = Intent(Intent.ACTION_SEND)
+            myIntent.data = (Uri.parse("mailto:" + getString(R.string.email)))
+            myIntent.type = "text/plain"
+            myIntent.setPackage("com.google.android.gm")
+            myIntent.putExtra(Intent.EXTRA_EMAIL, getString(R.string.email))
+            startActivity(Intent.createChooser(myIntent, "Share with"))
+        } else {
+            Toast.makeText(requireContext(), "App not Installed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isAppAvailable(appName: String?): Boolean {
+        return try {
+            requireContext().packageManager.getPackageInfo(appName!!, PackageManager.GET_ACTIVITIES)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+    }
 
 }
